ed2687195dfc60b90a543979d65f58860237b426
lgramirez <gramirez.developer@gmail.com>
2019-07-12 10:10:06 -0400

    Add clock component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Clock from './Clock.js';
 
 function App(props) {
   return (
@@ -19,6 +20,7 @@ function App(props) {
           Learn React
         </a>
         <p>{props.name}</p>
+        <Clock />
       </header>
     </div>
   );
