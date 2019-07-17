0546380fdcf7af505b3a84e1c2cb7e373d7e8fde
lgramirez <gramirez.developer@gmail.com>
2019-07-17 10:07:43 -0400

    Add validation for inputName

--- src/ProductForm.js
+++ src/ProductForm.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { red } from 'ansi-colors';
 
 const RESET_VALUES = {id: '', category: '', price: '', stocked: false, name: ''};
 
@@ -7,27 +8,54 @@ class ProductForm extends React.Component {
         super(props);
         this.handleSave = this.handleSave.bind(this);
         this.handleChange = this.handleChange.bind(this);
+        this.checkNameBeforeSubmit = this.checkNameBeforeSubmit.bind(this);
         this.state = {
             product: Object.assign({}, RESET_VALUES),
-            errors: {}
+            errors: {
+                message: "",
+                control: false
+            }
         };
     }
 
     handleSave(e) {
+        // prevent the form submit event from triggering an HTTP Post
+        e.preventDefault();
+        
+        if(this.checkNameBeforeSubmit()){
+            return;
+        }
+
         this.props.onSave(this.state.product);
         // reset form values to blank after submitting
         this.setState({
             product: Object.assign({}, RESET_VALUES),
-            errors: {}
+            errors: {
+                message: "",
+                control: false
+            }
         });
-        // prevent the form submit event from triggering an HTTP Post
-        e.preventDefault();
+    }
+
+    checkNameBeforeSubmit() {
+        if(this.state.product.name) {
+            this.setState({errors: {message: "", control: false}});
+            return false;
+        }
+        this.setState({errors: {message: "You have to send a name", control: true}});
+        return true;
     }
 
     handleChange(e) {
         const target = e.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
+
+        if(name === "name" && value){
+            this.setState({errors: {message: "", control: false}});
+        }else{
+            this.setState({errors: {message: "You have to send a name", control: true}});
+        }
 
         this.setState((prevState) => {
             prevState.product[name] = value;
@@ -38,12 +66,13 @@ class ProductForm extends React.Component {
     }
 
     render() {
+        let message = this.state.errors.control ? <span style={{color: 'red'}}>{this.state.errors.message}</span> : "";
         return (
             <form action="">
                 <h3>Enter a new product</h3>
                 <p>
                     <label>
-                        Name
+                        Name {message}
                         <br/>
                         <input type="text" name="name" value={this.state.product.name} onChange={this.handleChange} />
                     </label>
